1b1fff43e9b0ce213d9b17f5a0eeafce6a6c0492
jjaei <ieajnowk@gmail.com>
2023-08-18 03:33:54 +0900

    CategoryController 메서드 추가

--- src/main/java/com/example/memo/controller/CategoryController.java
+++ src/main/java/com/example/memo/controller/CategoryController.java
@@ -4,10 +4,7 @@ import com.example.memo.dto.CategoryDto;
 import com.example.memo.entity.Category;
 import com.example.memo.service.CategoryService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController  // 모든 데이터의 요청과 응답의 body를 json 형태로 전송
 @RequestMapping("/categories")
@@ -19,5 +16,10 @@ public class CategoryController {
     public Category createCategory(@RequestBody CategoryDto categoryDto) {
         // requestBody는 HTTP 요청의 body 부분을 가지고 오며 body의 내용을 UserDto에 담아서 처리해준다.
         return categoryService.createCategory(categoryDto);
+    }
+
+    @GetMapping("/{id}")
+    public Category getCategoryById(@PathVariable Long id) {  // URL 에 {}로 감싸져있음. -> /categories/1
+        return categoryService.getCategoryById(id);
     }
 }
