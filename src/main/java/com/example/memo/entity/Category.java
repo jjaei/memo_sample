f11aa052b9d7e11ac0aefa82a14261efadb5df88
jjaei <ieajnowk@gmail.com>
2023-08-18 02:44:06 +0900

    Category Entity 생성

--- src/main/java/com/example/memo/entity/Category.java
+++ src/main/java/com/example/memo/entity/Category.java
@@ -5,19 +5,18 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 
-@Entity
+@Entity // JPA에서 테이블 생성하고 테이블 데이터를 가지고 옴.
 @Getter
 @ToString
 public class Category {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id  // 기본키
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // 값을 자동으로 생성해주는 설정
     Long id;
 
-    @Column(length = 100, unique = true)
+    @Column(length = 100, unique = true) // DB Column
     String name;
 
     @Builder
-
     public Category(String name) {
         this.name = name;
     }
