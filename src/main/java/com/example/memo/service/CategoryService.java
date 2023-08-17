d9fc15c6962edaa9c745d69312e88d715b12475c
jjaei <ieajnowk@gmail.com>
2023-08-18 03:09:55 +0900

    CategoryService 생성

---
+++ src/main/java/com/example/memo/service/CategoryService.java
@@ -0,0 +1,34 @@
+package com.example.memo.service;
+
+import com.example.memo.dto.CategoryDto;
+import com.example.memo.entity.Category;
+import com.example.memo.repository.CategoryRepository;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor  // 모든 변수를 포함하는 생성자를 만들어줌.
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Transactional  // 예외가 발생하면 데이터베이스를 롤백해주는 기능 제공
+    public Category createCategory(CategoryDto categoryDto) {
+        // Repository에서 데이터 가져오기
+        Optional<Category> findOne = categoryRepository.findByName(categoryDto.getName());
+        if (findOne.isPresent()) { // 데이터가 이미 존재하면 예외 발생시키고 종료
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "중복된 이름입니다.");
+        }
+        Category category = Category.builder()
+                .name(categoryDto.getName())
+                .build();
+        category = categoryRepository.save(category); // 아니면 category 생성하기
+
+        return category;
+    }
+}
