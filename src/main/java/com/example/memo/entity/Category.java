ddd63751999aa2d382e99eb43da4b709198fccf6
jjaei <ieajnowk@gmail.com>
2023-08-18 15:27:53 +0900

    애노테이션 추가

--- src/main/java/com/example/memo/entity/Category.java
+++ src/main/java/com/example/memo/entity/Category.java
@@ -1,13 +1,12 @@
 package com.example.memo.entity;
 
 import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.ToString;
+import lombok.*;
 
 @Entity // JPA에서 테이블 생성하고 테이블 데이터를 가지고 옴.
 @Getter
 @ToString
+@NoArgsConstructor
 public class Category {
     @Id  // 기본키
     @GeneratedValue(strategy = GenerationType.IDENTITY)  // 값을 자동으로 생성해주는 설정
